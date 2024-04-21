82e7a102cfd31aeb9c9ebe22c7dd99a869892ff2
suraj-k-s <surajks28101999@gmail.com>
2024-04-21 06:01:53 +0530

    first commit

---
+++ lib/bloc/counter_bloc.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'counter_event.dart';
+part 'counter_state.dart';
+part 'counter_bloc.freezed.dart';
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(CounterState.initial()) {
+    on<Increment>((event, emit) {
+      return emit(state.copyWith(count: state.count+1));
+    });
+
+    //Both ways are emiting the values
+    on<Decrement>((event, emit) {
+     final newState=state.copyWith(count: state.count-1);
+     return emit(newState);
+    });
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_counter/bloc/counter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => CounterBloc(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            BlocBuilder<CounterBloc, CounterState>(
+              builder: (context, state) {
+                return Text(
+                  state.count.toString(),
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FloatingActionButton(
+        onPressed: () {
+          context.read<CounterBloc>().add(const Increment());
+        },
+       
+        child: const Icon(Icons.add),
+      ),
+      FloatingActionButton(
+        onPressed: () {
+          context.read<CounterBloc>().add(const Decrement());
+        },
+       
+        child: const Icon(Icons.remove),
+      ),
+        ],
+      )
+    );
+  }
+}
